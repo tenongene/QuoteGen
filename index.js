c38cca6b4a8f4e4099d4fa208783c4662930b748
Tenongene <tenongene00@gmail.com>
2023-02-13 15:44:19 -0600

    rendering complete

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { json } = require('body-parser');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+//to generate random index numbers from json object returned from API calls
 function randomIndex(max) {
 	return Math.floor(Math.random() * max);
 }
@@ -15,21 +17,25 @@ app.set('view engine', 'ejs');
 app.use(express.static('assets'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	const url = 'https://zenquotes.io/api/random';
-	const url_pic = `https://api.pexels.com/v1/search?query=nature&per_page=80&page=${randomIndex(100)}`;
+//API Urls
+const url_pic = `https://api.pexels.com/v1/search?query=nature&per_page=80&page=${randomIndex(100)}`;
+const url = 'https://zenquotes.io/api/random';
+
+//Get route with sequential ajax calls for quote and image
+app.get('/', (req, res, next) => {
 	fetch(url, { method: 'GET' })
 		.then((response) => response.json())
 		.then((json) => {
-			res.render('index', { quote: json[0].q, author: json[0].a });
-			console.log(json[0].q, json[0].a);
-		})
-		.catch((err) => console.error('error:' + err));
+			const quote = json[0].q;
+			const author = json[0].a;
 
-	fetch(url_pic, { method: 'GET' })
-		.then((response) => response.json())
-		.then((json) => {
-			res.render('index', { photo: json.photos[randomIndex(79)].src.large2x });
-		})
-		.catch((err) => console.error('error:' + err));
+			fetch(url_pic, { method: 'GET' })
+				.then((response) => response.json())
+				.then((json) => {
+					const photo = json.photos[randomIndex(79)].src.large;
+					const alt = json.photos[randomIndex(79)].src.alt;
+					res.render('index', { quote, author, photo, alt });
+				})
+				.catch((err) => console.log(err.message));
+		});
 });
